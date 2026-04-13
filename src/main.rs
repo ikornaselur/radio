cffe88cdddcc93f149be4b412cd02d884152be57
Axel Örn Sigurðsson <git@absalon.dev>
2026-04-13 21:19:34 +0200

    Add basic keyboard tuning for testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use device_query::{DeviceQuery, DeviceState};
+use device_query::{DeviceQuery, DeviceState, Keycode};
 use display_info::DisplayInfo;
 use radio::{StationManager, load_config};
 
@@ -15,9 +15,20 @@ fn main() -> Result<()> {
     let screen_width = DisplayInfo::all()?[0].width as f32;
     log::debug!("Screen width: {screen_width}");
     let device_state = DeviceState::new();
+    let mut dial = 0.5;
     loop {
-        let (x, _) = device_state.get_mouse().coords;
-        let dial = (x as f32 / screen_width).clamp(0.0, 1.0);
+        // let (x, _) = device_state.get_mouse().coords;
+        // let dial = (x as f32 / screen_width).clamp(0.0, 1.0);
+        let keys = device_state.get_keys();
+
+        let mut dial_delta = 0.0;
+        if keys.contains(&Keycode::Left) {
+            dial_delta -= 0.001;
+        }
+        if keys.contains(&Keycode::Right) {
+            dial_delta += 0.001;
+        }
+        dial += dial_delta;
 
         manager.tick(dial)?;
     }
